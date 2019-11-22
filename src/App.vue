a41999aa3d2c4d4fef354871dcdb790fdf5f0339
Ruhel <ruhel241@gmail.com>
2019-11-22 16:39:07 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+      
+  <!-- Navigation -->
+  <Navber></Navber> <br/><br/><br/>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-9">
+              <!-- Home slider -->
+                <HomeSlider></HomeSlider>
+              <!-- Inventory -->
+              <Inventory :items="items"></Inventory>
+          </div>
+      
+          <div class="col-lg-3">
+              <!-- Cart -->
+              <Cart></Cart>
+          </div>
+        </div>
+    </div>
+  
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+      </div>
+    </footer>
+
+  </div>
+</template>
+
+<script>
+
+import Navber from './components/Navber'
+import Inventory from './components/Inventory'
+import Cart from './components/Cart'
+import HomeSlider from './components/HomeSlider'
+import data from './data.js'
+
+export default {
+
+    components:{
+      Navber,
+      Inventory,
+      Cart,
+      HomeSlider
+    },
+
+    data(){
+      return{
+        items: []
+      }
+    },
+
+    mounted(){
+      this.items = data;
+      //console.log(this.items);
+    }
+
+
+}
+</script>
+<style>
+#customers{
+  width: 100%;
+}
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+</style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1 class="my-4">Total Cart</h1>
+            <table id="customers">
+                <tr>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Delete</th>
+                </tr>
+                <tr>
+                  <td>Item one</td>
+                  <td>$112.2</td>
+                  <td><button class="btn btn-sm btn-danger"> --</button></td>
+                </tr>
+                  <tr>
+                  <td>Item one</td>
+                  <td>$112.2</td>
+                  <td><button class="btn btn-sm btn-danger"> --</button></td>
+                </tr>
+                  <tr>
+                  <td>Item one</td>
+                  <td>$112.2</td>
+                  <td><button class="btn btn-sm btn-danger"> --</button></td>
+                </tr>
+                <tr>
+                  <th>Total</th>
+                  <th>$500</th>
+                  <th></th>
+                </tr>
+            </table>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="stylus" scoped>
+
+</style>
